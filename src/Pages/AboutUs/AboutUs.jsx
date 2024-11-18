d9d1aa33c3c2f597047338292727a3099a6273c8
mdnobin96670 <farhanahmednobin@gmail.com>
2024-11-18 19:03:13 +0600

    DN AboutUs page

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,18 +1,28 @@
 import fetchRoutine from "../../Hooks/fetchRoutine";
+import { useState } from "react";
+import About from "./About/About";
+import Campus from "./Campus/Campus";
+import Hero from "./Hero/Hero";
+import Programs from "./Programs/Programs";
+import Testimonials from "./Testimonials/Testimonials";
+import Title from "./Title/Title";
+import VideoPlayer from "./Video player/VideoPlayer";
 
 const AboutUs = () => {
-  const { data: HomeBanners, content } = fetchRoutine();
-
-  // Render loading or error content if present
-  if (content) {
-    return content;
-  }
-
-  // If data is successfully fetched, render the About Us content
-  console.log(HomeBanners);
+  const [playState, setPlaystate] = useState(false);
   return (
     <div>
-      <p>This is About Us page </p>
+      <Hero />
+      <div className="container">
+        <Title subTitle="OUR PROGRAM " title="What We Offer" />
+        <Programs />
+        <About setPlaystate={setPlaystate} />
+        <Title subTitle="Gallary " title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS " title="What Student Says" />
+        <Testimonials />
+      </div>
+      <VideoPlayer playState={playState} setPlaystate={setPlaystate} />
     </div>
   );
 };
