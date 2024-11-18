4a1d68c10b8a1625169d9dcceb9a1c72adcc2742
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2024-11-18 12:39:40 +0600

    some changes

--- src/Pages/Academic/Academic.jsx
+++ src/Pages/Academic/Academic.jsx
@@ -9,6 +9,7 @@ import {
   FaTwitter,
 } from "react-icons/fa";
 import Gallery from "../../Components/Gallary/Gallery";
+import DepartmentBanner from "../../Components/Banners/DepartmentBanner";
 
 const Academic = () => {
   const GalleryImages = [
@@ -48,33 +49,22 @@ const Academic = () => {
 
   return (
     <div className="text-black">
+      <DepartmentBanner />
       {/* Main Container */}
       <div className="gap-5">
         {/* Top Part */}
-        <div className="max-w-[1200px] mx-auto px-5">
-          <div className="flex flex-col md:flex-row items-center gap-5 w-full">
-            <p className="text-[128px] font-semibold">12+</p>
-            <p className="text-3xl md:text-5xl font-semibold text-center md:text-left">
-              BTEB Affiliated Courses
-            </p>
+        <div className="max-w-[1200px] mx-auto">
+          <div className="flex flex-col md:flex-row items-center w-full">
+            <p className="text-[128px] font-semibold text-center">12+ <span className="text-3xl md:text-5xl font-semibold md:text-left">BTEB Affiliated Courses</span></p>
           </div>
-
           {/* Top text-1 */}
-          <p className="pt-12 text-[#7A7A7A] text-lg font-medium md:px-5 text-center md:text-left">
-            The computer course offered at our institute is designed to equip
-            students with essential skills in both theoretical and practical
-            aspects of computing...
-          </p>
-
-          {/* Top text-2 */}
-          <p className="pt-6 text-[#7A7A7A] text-lg font-medium md:px-5 text-center md:text-left">
-            In the advanced stages, the computer course delves into programming,
-            web development, and data analysis...
+          <p className="text-[#7A7A7A] text-lg font-medium md:px-5 text-center md:text-left">
+          The Bangladesh Technical Education Board (BTEB) offers a range of 12+ vocational and technical education programs to equip students with practical skills and industry-specific knowledge. These courses are designed for students who have completed their secondary education and wish to pursue specialized training in fields like engineering, technology, healthcare, design, and hospitality.
           </p>
         </div>
 
         {/* Gallery */}
-        <div className="my-32">
+        <div className="my-32 px-32">
           <Gallery images={GalleryImages} />
         </div>
 
@@ -85,22 +75,28 @@ const Academic = () => {
             alt="Banner"
             className="w-full h-[300px] sm:h-[400px] md:h-[500px] object-cover"
           />
-          <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 bg-white w-full sm:max-w-[900px] p-10 border border-gray-60">
+          <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 bg-white w-full sm:max-w-[900px] p-5 border border-gray-60">
             <p className="text-black text-2xl md:text-3xl font-semibold text-center">
               Graduate Areas of Study
             </p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 pt-5 overflow-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-2 pt-5">
               {[
-                "Business",
-                "Engineering",
-                "Health",
-                "Science",
-                "Arts",
-                "Law",
-                "Education",
-                "Social Sciences",
-                "Technology",
-                "Environment",
+                "Refrigeration and Air Conditioning Technology",
+                "Computer Technology",
+                "Architecture and Interior Design Technology",
+                "Electronics Technology",
+                "Civil Technology",
+                "Mechanical Technology",
+                "Power Technology",
+                "Ceramic Technology",
+                "Surveying Technology",
+                "Automobile Technology",
+                "Garment Design and Pattern Making",
+                "Food Technology",
+                "Data Communication and Networking",
+                "Telecommunication Technology",
+                "Graphic Design Technology",
+                "Printing Technology",
               ].map((tag, index) => (
                 <button
                   key={index}
@@ -139,16 +135,16 @@ const Academic = () => {
       </div>
 
       {/* Bottom Part */}
-      <div className="bg-[#2e465e] mt-28 py-10 text-center">
+      {/* <div className="bg-[#2e465e] mt-28 py-10 text-center">
         <img src={school} alt="school" className="mx-auto" />
         <div>
           <p className="text-2xl text-[#B9B9BA] w-[90%] max-w-[800px] mx-auto mb-8">
             University Life Nurtures an Inclusive Campus Life Environment Where
             Students Grow Intellectually...
-          </p>
+          </p> */}
 
-          {/* Social Media Icons */}
-          <div className="flex justify-center gap-3 flex-wrap">
+      {/* Social Media Icons */}
+      {/* <div className="flex justify-center gap-3 flex-wrap">
             <p className="bg-[#073972] hover:bg-green-500 transition-colors duration-700 text-white px-4 py-4">
               <FaFacebookF className="text-xl" />
             </p>
@@ -163,7 +159,7 @@ const Academic = () => {
             </p>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
