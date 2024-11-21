04c5adeb9f0e10f5d9f6ae9eb0e469b7f283a16f
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2024-11-21 07:33:12 +0600

    add

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -2,7 +2,6 @@ import DepartmentBanner from "../../Components/Banners/DepartmentBanner";
 import LeftTitle from "../../Components/ReuseableTitle/leftTitle";
 import AboutPrincipal from "./AboutPrincipal/AboutPrincipal";
 import AboutUniversity from "./AboutUniversity/AboutUniversity";
-import Academics from "./Academics/Academics";
 import BannerImage from "../../assets/AboutUsBanner.jpg"
 import Staff from "./Staff/Staff";
 
@@ -120,7 +119,7 @@ const AboutUs = () => {
       <AboutPrincipal principalData={principalData}></AboutPrincipal>
 
       <Staff staffData={staffData}></Staff>
-      <Academics academicServices={academicServices}></Academics>
+      {/* <Academics academicServices={academicServices}></Academics> */}
       
     </div>
   );
